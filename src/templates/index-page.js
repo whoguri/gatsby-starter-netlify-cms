bcd58a9d31607d646fecc6d5cf4d50840ae4b049
Gurwinder Singh <40499496+whoguri@users.noreply.github.com>
2022-03-03 06:37:42 -0800

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -22,7 +22,7 @@ export const IndexPageTemplate = ({
 
   return (
     <div>
-      <FullWidthImage img={heroImage} title={title} subheading={subheading} />
+      <FullWidthImage img={heroImage} title={"XXX "+title} subheading={subheading} />
       <section className="section section--gradient">
         <div className="container">
           <div className="section">
